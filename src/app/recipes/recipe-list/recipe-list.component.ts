2271a64fcaacfe8798d2c4f62159d6af52a798a7
Bruno Toffolo <brunotoffolo@gmail.com>
2016-11-27 23:24:46 -0200

    Create RecipeList and RecipeItem components
    This commit introduces the components needed to populate the
    recipes list displayed at the left side of the application.

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe';
+
+@Component({
+  selector: 'rb-recipe-list',
+  templateUrl: './recipe-list.component.html'
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
+
+  // TODO Replace with real recipe list
+  recipe = new Recipe("Hamburger",
+              "Delicious hamburger recipe",
+              "http://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/6:4/w_620,h_413/the-ultimate-hamburger.jpg");
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
