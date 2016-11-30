b4c1bfabc9de02f1212ef4b7c355443549bcdfc6
Bruno Toffolo <brunotoffolo@gmail.com>
2016-11-30 21:10:01 -0200

    Create the Ingredient component and use ngFor
    The existing lists in the application were replaced by the ngFor
    directive to be dynamically generated at runtime by Angular so the
    actual contents of the arrays are shown.
    
    The Ingredient class was created to represent this entity and a
    reference to it was included in the Recipe class.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,15 +7,23 @@ import { Recipe } from '../recipe';
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe("Hamburger",
+              "Delicious hamburger recipe",
+              "http://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/6:4/w_620,h_413/the-ultimate-hamburger.jpg",
+              []),
+    new Recipe("Schnitzel",
+              "Great if it is not made of pork",
+              "http://kmetro.restaurantden.com/wp-content/uploads/sites/67/2016/03/dish_1.jpg",
+              []),
+    new Recipe("Burrito",
+              "Oh Mexico, how I miss your food...",
+              "http://www.myburritobros.com/images/making-steak-burritos2.jpg",
+              [])
+  ];
 
   // TODO Improve later
   @Output() recipeSelected = new EventEmitter<Recipe>();
-
-  // TODO Replace with real recipe list
-  recipe = new Recipe("Hamburger",
-              "Delicious hamburger recipe",
-              "http://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/6:4/w_620,h_413/the-ultimate-hamburger.jpg");
 
   constructor() { }
 
