c3ab051592d3cb92a3b8f1bdc0afcaae00d5b822
Bruno Toffolo <brunotoffolo@gmail.com>
2016-12-08 12:10:52 -0200

    Auto-load recipes on start up
    A small hook was implemented to allow the persisted recipes to get
    loaded from Firebase at the application start up. When the recipe
    service's getRecipes() is called for the first time, it will load
    the entries from database so the array is correctly populated.
    
    As this method now works totally integrated with Firebase, the fake
    (mocked) recipes were removed from the app. Now it works 100% with
    real information present in the database.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,35 +9,16 @@ import { Ingredient } from '../shared/ingredient';
 @Injectable()
 export class RecipeService {
   recipesChanged = new EventEmitter<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe("Hamburger",
-              "Delicious hamburger recipe",
-              "http://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/6:4/w_620,h_413/the-ultimate-hamburger.jpg",
-              [
-                new Ingredient('Bread', 2),
-                new Ingredient('Meat', 1),
-                new Ingredient('Cheese', 1)
-              ]),
-    new Recipe("Schnitzel",
-              "Great if it is not made of pork",
-              "http://kmetro.restaurantden.com/wp-content/uploads/sites/67/2016/03/dish_1.jpg",
-              [
-                new Ingredient('French fries', 2),
-                new Ingredient('Chicken breast', 1)
-              ]),
-    new Recipe("Burrito",
-              "Oh Mexico, how I miss your food...",
-              "http://www.myburritobros.com/images/making-steak-burritos2.jpg",
-              [
-                new Ingredient('Rice', 1),
-                new Ingredient('Beans', 1),
-                new Ingredient('Meat', 2)
-              ])
-  ];
+  private recipes: Recipe[] = [];
+  private recipesInitialized: boolean = false;
 
   constructor(private http: Http) { }
 
   getRecipes() {
+    if (!this.recipesInitialized) {
+      this.fetchData();
+      this.recipesInitialized = true;
+    }
     return this.recipes;
   }
 
@@ -74,6 +55,7 @@ export class RecipeService {
                  (data: Recipe[]) => {
                    this.recipes = data;
                    this.recipesChanged.emit(this.recipes);
+                   console.log(data);
                  }
                );
   }
