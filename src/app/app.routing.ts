965cba2a6c71ca145b053b1b4077c8b64e4592d8
Bruno Toffolo <brunotoffolo@gmail.com>
2016-12-11 17:35:21 -0200

    Use feature modules and lazy loading of routes
    Introduce two feature modules to aggregate the content of Recipes
    and Shopping List modules, respectively, so the main AppModule can
    be decluttered and code is less coupled (and messy).
    
    Also introduce lazy loading of routes to allow the modules to be
    more independent. This also allows performance to have be slightly
    improved by only loading what is necessary for the current app
    usage.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 
-import { RecipesComponent } from './recipes/recipes.component';
+import { HomeComponent } from './home.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RECIPE_ROUTES } from './recipes/recipes.routes';
 
 const APP_ROUTES: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'recipes', component: RecipesComponent, children: RECIPE_ROUTES },
-    { path: 'shopping-list', component: ShoppingListComponent }
+    { path: '', component: HomeComponent },
+    { path: 'recipes', loadChildren: 'app/recipes/recipes.module#RecipesModule' },
+    { path: 'shopping-list', loadChildren: 'app/shopping-list/shopping-list.module#ShoppingListModule' }
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
